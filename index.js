c3a20859f730a90e7345828bb7224038410e3785
AngelFavela <favelaa687@gmail.com>
2023-08-29 12:56:19 -0600

    se agrego

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hola");
+let a=2, b=5, r, s="exponente= "
+r= a<<=16
+const persona = {
+    nombre:"angel", apellidoPaterno:"favela", apellidoMaterno:"duran",
+    semestre:7
+}
+const fruta=["naranja","manzana","platano","tomate",2, new Date(), 2.5,{
+    nombre:"angel", apellidoPaterno:"favela", apellidoMaterno:"duran",
+    semestre:7
+}]
+//console.log(fruta[7].nombre, fruta[5])
+function promedio(u1,u2,u3,u4){
+    let prom = (u1+u2+u3+u4)/4
+    return prom
+}
+console.log(promedio(8,9,7,9.5))
---
+++ t1.js
@@ -0,0 +1,40 @@
+var libro = {
+    titulo: "El Gran Gatsby",
+    autor: "F. Scott Fitzgerald",
+    añoPublicacion: 1925,
+    genero: "Novela",
+    paginas: 218,
+    estado: "Disponible",
+    
+    prestar: function(persona) {
+      if (this.estado === "Disponible") {
+        this.estado = "Prestado a " + persona;
+        return "El libro ha sido prestado a " + persona;
+      } else {
+        return "El libro no está disponible para préstamo en este momento.";
+      }
+    },
+    
+    devolver: function() {
+      if (this.estado.includes("Prestado")) {
+        this.estado = "Disponible";
+        return "El libro ha sido devuelto y ahora está disponible para préstamo.";
+      } else {
+        return "El libro no se encuentra prestado en este momento.";
+      }
+    },
+    
+    informacion: function() {
+      return "Título: " + this.titulo + "\n" +
+             "Autor: " + this.autor + "\n" +
+             "Año de Publicación: " + this.añoPublicacion + "\n" +
+             "Género: " + this.genero + "\n" +
+             "Páginas: " + this.paginas + "\n" +
+             "Estado: " + this.estado;
+    }
+  };
+  
+  console.log(libro.prestar("Juan"));
+  console.log(libro.informacion());
+  console.log(libro.devolver());
+  
