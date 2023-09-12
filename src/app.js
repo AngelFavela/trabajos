e76fa0d051ce915a42d99cc6b9a1a3c90a4385d8
AngelFavela <favelaa687@gmail.com>
2023-09-12 15:54:49 -0600

    creamos la conexion a la bd

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require('express'); //referencia a framework express
 const app = express(); //se conecta la apliccacion a express
 const log = require('morgan'); //para saber los clientes conectados
 const bodyParser = require('body-parser');
+const path = require('path');
 const IndexRouters = require('./routers/index.js');
+const { default: mongoose } = require('mongoose');
+
 app.set('port', process.env.PORT || 3000); // asigna puerto 3000
 
 //middleware utiliza morgan
@@ -14,4 +17,13 @@ app.use('/',IndexRouters);
 app.listen(app.get('port'), () =>{
     console.log('el servidor ta funcionando en el puerto', app.get('port'));
     }
-    ); //despliega mensaje de conectado
+    );
+    mongoose.connect("mongodb+srv://node:favela03@cluster0.wxos6ze.mongodb.net/punto-venta?retryWrites=true&w=majority")
+    .then(bd=>console.log('bd conectada'))
+    .catch(err=>console.log(err));
+
+    //establecer sistema de ventas
+    app.set('views',path.join(__dirname,'views'));
+    app.set('view engine', 'ejs');
+    
+    //despliega mensaje de conectado
---
+++ src/models/ventas.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const Venta = new Schema(
+    {
+        producto: String,
+        precio : String,
+        iva: String
+    }
+);
+
+module.exports = mongoose.model('datos', Venta);
