35ddc802f868db5a039d1461e784fb4026a00d34
AngelFavela <favelaa687@gmail.com>
2023-09-18 12:04:38 -0600

    se agrego el modal al boton elminar

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,30 +3,30 @@ const router = express.Router();
 const model = require('../models/ventas')();
 const Venta = require('../models/ventas')
 
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) =>{
     const datos = await Venta.find();
     console.log(datos);
-    res.render('index.ejs',
-        {
-            datos
-        });
-    
+    res.render('index.ejs',{
+        datos
+    });
 });
-router.post('/add', async (req,res)=>{
-    const Valor = new Venta(req.body);
-    console.log(req.datos);
-    await Valor.save();
-    res.redirect('/');
 
-    
+
+router.post('/add', async (req, res) => {
+    const valor = new Venta(req.body);
+    console.log(req.body);
+    await valor.save();
+    res.redirect('/');
 });
-router.get('/del/:id', async (req,res)=>{
-    const {id} = req.params;
+
+
+router.get('/del/:id', async(req,res)=>{
+    const {id}=req.params;
     await Venta.findByIdAndRemove(id);
     res.redirect('/');
 
-    
-});
+})
+
 
 module.exports = router;
 
